51ec1978602372402143a3c5c0b23547f6752e1d
Robin Rheem <robinrheem@gmail.com>
2022-12-21 18:35:48 -0800

    Fix dashboard rename error

--- wirebus/dashboard/apps.py
+++ wirebus/dashboard/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DashappConfig(AppConfig):
+class DashboardConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "dashapp"
+    name = "dashboard"
