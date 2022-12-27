609cfc951d9c5d5eb1fd1f74ad51590a4c87ef8d
taejun <songtj10@snu.ac.kr>
2022-12-27 18:59:14 +0900

    add django-js-lib-ag-grid-community

--- wirebus/dashboard/urls.py
+++ wirebus/dashboard/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from dashboard import views, consumer
-
+from . import views
 
 urlpatterns =[
-    path('', views.index , name="home"),
+    path('', views.index, name="home"),
 ]
 ws_urlpatterns = [
     path('ws/tableData/', consumer.TableData.as_asgi()),
--- wirebus/wirebus/settings.py
+++ wirebus/wirebus/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "js_lib_ag_grid_community",
     "daphne",
     "channels",
     "dashboard.apps.DashboardConfig",
@@ -134,6 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = ["wirebus/static",]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
