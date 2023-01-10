3f68318333163ba2e2706bd36375dfa7d4d48104
taejun-song <songtj10@snu.ac.kr>
2023-01-10 20:57:19 +0900

    [Server-side]connect binance websocket

--- wirebus/dashboard/consumer.py
+++ wirebus/dashboard/consumer.py
@@ -1,27 +1,38 @@
+import json
+
+import websockets
+
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-class TableData(AsyncWebsocketConsumer):
+class BinanceConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.group_name='tableData'
+        self.group_name = 'binance'
         await self.channel_layer.group_add(
             self.group_name,
             self.channel_name
         )
         await self.accept()
+        ticker = 'btcusdt'
+        ws_url = f'wss://stream.binance.com:9443/ws/{ticker}@bookTicker'
+        async with websockets.connect(ws_url) as ws:
+            async def receive_binance_updates():
+                async for msg in ws:
+                    data = json.loads(msg)
+
+                    await self.channel_layer.group_send(self.group_name,{
+                            'type': 'update_data',
+                            'data': {'exchange':'Binance','price':data['a']},
+                        })
+
+            await receive_binance_updates()
 
     async def disconnect(self, close_code):
+        await ws.close()
 
-        await self.channel_layer.group_discard(self.group_name,
-                                               self.channel_name)
+    async def update_data(self, event):
+        data = event['data']
 
-    async def receive(self, text_data):
-        await self.channel_layer.group_send(
-            self.group_name,
-            {
-                'type': 'randomFunction',
-                'value': text_data,
-            }
-        )
-
-    async def randomFunction(self, event):
-        await self.send(event['value'])
+        await self.send(text_data=json.dumps({
+            'exchange': data['exchange'],
+            'price': data['price']
+        }))
